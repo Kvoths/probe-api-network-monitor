1bcfcfd3fab23a316c31e70ea2cd38db8ec126da
carlos <carloslopez@nitsnets.com>
2019-05-10 12:54:15 +0200

    Variables de entorno añadidas

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
-var mqtt = require('mqtt')
-var client  = mqtt.connect('mqtt://localhost')
+//Cargar configuración de variables
+require('dotenv').config();
+
+const mqtt = require('mqtt');
+let client  = mqtt.connect(`mqtt://${(process.env.MOSQUITTO_URL) ? process.env.MOSQUITTO_URL : 'localhost'}`);
  
 client.on('connect', function () {
-  client.subscribe('presence', function (err) {
+  client.subscribe(`probe/${process.env.PROBE_ID}`  , function (err) {
     if (!err) {
-      client.publish('presence', 'Hello mqtt')
+      client.publish(`probe/${process.env.PROBE_ID}`, 'Hello mqtt');
+    } else {
+        console.log(err);
     }
+
   })
 });
  
 client.on('message', function (topic, message) {
   // message is Buffer
-  console.log(message.toString())
-  //client.end()
+  console.log(message.toString());
+  //client.end();
 });
